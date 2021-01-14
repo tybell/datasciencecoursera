e263d7d46300d5a80ed7cf62d7a636494415d20e
Tyler Bell <tylerbell.nyc@gmail.com>
2021-01-14 10:40:20 -0500

    Read github repo data using github API

---
+++ Github_API.R
@@ -0,0 +1,36 @@
+library(httr)
+library(httpuv)
+library(tidyjson)
+
+# Register Oauth Application
+# Github -> Developer Settings -> OAuth Apps -> New
+# Homepage URL <- http://github.com
+# Authorization callback URL <- http://localhost:1410 (1410 is httr default port)
+
+application_name <- 'TBCoursera'
+client_id <- 'cb9ad898bf74b7962853'
+client_secret <- '6a9230bbdc736d533cbc0124b0e303050de71f1f'
+
+my_github_app <- oauth_app(appname = application_name, 
+                           key = client_id, 
+                           secret = client_secret)
+
+github_token <- config(token = oauth2.0_token(oauth_endpoints('github'), my_github_app))
+
+#Send request to jtleek's repo using github API
+request <- GET("https://api.github.com/users/jtleek/repos", github_token)
+
+#Check for http errors
+stop_for_status(request)
+
+# Retrieve contents of page
+# JSON is common format used by APIs, and is indeed the case here
+data_json = content(request)
+
+#Convert data to tbl_json
+data_tibble <- spread_all(data_json)
+
+#Find datasharing repo and access created_at attribute
+date_created <- data_tibble[data_tibble$full_name == "jtleek/datasharing",'created_at']$created_at
+
+print(date_created)
