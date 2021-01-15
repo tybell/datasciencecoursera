70b396c281d1f72a3b9ec8daf6c7e3222c1e7a28
tybell <tylerbell.nyc@gmail.com>
2021-01-14 19:40:23 -0500

    Update Github_API.R
    #Replaced variable name data_json with api_data

--- Github_API.R
+++ Github_API.R
@@ -11,10 +11,10 @@ application_name <- 'TBCoursera'
 client_id <- 'cb9ad898bf74b7962853'
 client_secret <- '6a9230bbdc736d533cbc0124b0e303050de71f1f'
 
+#Create github app and token
 my_github_app <- oauth_app(appname = application_name, 
                            key = client_id, 
                            secret = client_secret)
-
 github_token <- config(token = oauth2.0_token(oauth_endpoints('github'), my_github_app))
 
 #Send request to jtleek's repo using github API
@@ -24,11 +24,11 @@ request <- GET("https://api.github.com/users/jtleek/repos", github_token)
 stop_for_status(request)
 
 # Retrieve contents of page
-# JSON is common format used by APIs, and is indeed the case here
-data_json = content(request)
+# JSON is a common output format used by APIs, and is indeed the case here
+api_data = content(request)
 
 #Convert data to tbl_json
-data_tibble <- spread_all(data_json)
+data_tibble <- spread_all(api_data)
 
 #Find datasharing repo and access created_at attribute
 date_created <- data_tibble[data_tibble$full_name == "jtleek/datasharing",'created_at']$created_at
